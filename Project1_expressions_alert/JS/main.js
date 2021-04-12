14c8eea6f18d68ed84e5826bd6ee9bee03e36d7d
Josue-23 <79769933+Josue-23@users.noreply.github.com>
2021-04-12 12:13:44 -0600

    My JavaScript
    Doing some basics

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+alert("i'm a window")
+var family="jonsons", daughter="makayla", son="jonah"
+document.write(daughter);
+
+document.write(" is the daughter,")
+
+
+document.write( son)
+document.write("    is the son")
+
+
+var a ="The perfect X.";
+var a = a.fontcolor("blue");
+document.write(a);
+
+document.write(8+5);
+
+
+//These are my basics.//
+
+
+
+
+
