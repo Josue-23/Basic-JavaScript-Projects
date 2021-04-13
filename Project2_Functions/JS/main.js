5e9210e68757a9a85cf5c7c96887168e998ec204
Josue-23 <79769933+Josue-23@users.noreply.github.com>
2021-04-12 18:24:42 -0600

    linking my html to a JavaScript
    This my function.

---
+++ Project2_Functions/JS/main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
+
+//This is creating an +=operator in this file so and it calls it to a function//
