b0da801827527b081c9e56f2dd7d9fdfb810e2ec
Josue-23 <79769933+Josue-23@users.noreply.github.com>
2021-04-20 03:27:29 -0600

    Getting used to the logical operators
    Working on all 3 of the logical operators and other expressions!

---
+++ Projects5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,25 @@
+document.write(typeof 3);
+document.write("</br>");
+
+document.write(10>2);
+document.write("</br>");
+
+document.write(3===4);
+document.write("</br>");
+
+document.write(10==10);
+document.write("</br>");
+
+document.write(5 > 2 && 10 > 4);
+document.write("</br>");
+
+document.write(5 > 2 || 10 > 4);
+document.write("</br>");
+
+function not_function() {
+    document.getElementById("Not").innerHTML=!(20>10);
+}
+
+x=10
+y=50
+document.write(x-y);
