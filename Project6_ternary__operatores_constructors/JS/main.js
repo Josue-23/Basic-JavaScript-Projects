630aa40b45980d9b2502e208d59770dd9eaf03e2
Josue-23 <79769933+Josue-23@users.noreply.github.com>
2021-04-26 18:26:53 -0600

    doing me ternary
    Adding my functions

--- Project6_ternary__operatores_constructors/JS/main.js
+++ Project6_ternary__operatores_constructors/JS/main.js
@@ -4,3 +4,29 @@ function Ride_Function() {
     Can_ride = (height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+//This is for a ride!//
+
+function Animal(Type, Color, Year) {
+    this.Animal_Type = Type;
+    this.Animal_Year = Year;
+    this.Animal_Color = Color;
+}
+var Tigger = new Animal("Tiger", 1995, "orange");
+var DOv    = new Animal("Wolf", 2002, "black")
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML = 
+    "tigger is a " + Tigger.Animal_Type + "-colored- " + Tigger.Animal_Year + 
+    "born in " + Tigger.Animal_Color;
+}
+
+//This is a ternary//
+
+function count_Function() {
+    document.getElementById("Counting").innerHTML= Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point =+1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
